263dd42269b53e5779f61051c54cfea138267867
alomakin <artem@lomakin.com>
2021-11-29 02:40:06 +0100

    Corrected hierarchical_logist to work with only one annotation layer (no hierarchy)

--- schierarchy/logistic/_logistic_module.py
+++ schierarchy/logistic/_logistic_module.py
@@ -65,9 +65,14 @@ class HierarchicalLogisticPyroModel(PyroModule):
     @property
     def layers_size(self):
         if self.tree is not None:
-            return [len(x) for x in self.tree] + [
-                len([item for sublist in self.tree[-1].values() for item in sublist])
-            ]
+            if len(self.tree) > 1:
+                return [len(x) for x in self.tree] + [
+                    len(
+                        [item for sublist in self.tree[-1].values() for item in sublist]
+                    )
+                ]
+            else:
+                return [len(self.tree[0])]
         else:
             return None
 
