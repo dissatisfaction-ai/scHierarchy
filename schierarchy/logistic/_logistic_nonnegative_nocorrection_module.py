ebe5ef75e2b0930600197ef25fcee68f3b553e45
Vitalii Kleshchevnikov <klv2706w@gmail.com>
2021-12-28 01:53:49 +0000

    fixed default prior names

--- schierarchy/logistic/_logistic_nonnegative_nocorrection_module.py
+++ schierarchy/logistic/_logistic_nonnegative_nocorrection_module.py
@@ -26,7 +26,7 @@ class HierarchicalLogisticPyroModel(PyroModule):
         n_cells_per_label_per_level,
         tree,
         weights_prior={"alpha": 0.1, "beta": 1.0, "alpha_hierarchical": 0.3},
-        bias_prior={"mean": -10.0, "sigma": 3.0},
+        bias_prior={"mu": -10.0, "sigma": 3.0},
         learning_mode="fixed-sigma",
         learn_alpha: bool = False,
         init_vals: Optional[dict] = None,
